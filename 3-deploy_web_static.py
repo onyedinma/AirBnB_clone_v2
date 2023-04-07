07fe8b83d98ecb84e94f998f3d3d503b2ff71070
Nginika <nginikaugochukwu@gmail.com>
2023-04-07 01:07:59 +0000

    deploy full webstatic

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,8 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["52.87.10.77", "34.229.255.211"]
+env.user = "ubuntu"
 
 
 def do_pack():
