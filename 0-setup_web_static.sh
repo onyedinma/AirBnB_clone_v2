63413b10f5bfa00dc65a9e51aff3843dee7375d4
KachKreative <onyedinmaonyekachi@yahoo.com>
2024-01-17 20:26:29 +0100

    Webstatic 2.0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
+
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+service nginx restart
